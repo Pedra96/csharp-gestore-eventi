09d5f7b8e0397a24c14a4cc8d0a2a43280d714f9
Pedra96 <darkraidialga@yahoo.it>
2022-12-13 11:02:28 +0100

    Milestone 1 completata,aggiunti try catch per debugging

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi {
+    public class Evento {
+        private string titolo;
+        private DateTime data;
+        private int CapienzaMax;
+        private int PostiPrenotati;
+
+        public Evento(string titolo, string data, int CapienzaMax) {
+
+            SetTitolo(titolo);
+            SetData(data);
+            this.CapienzaMax=CapienzaMax;
+            PostiPrenotati = 0;
+
+        }
+        public string GetTitolo() {
+            return titolo;
+        }
+        public void SetTitolo(string titolo) {
+            if (string.IsNullOrWhiteSpace(titolo)) {
+                throw new Exception("Il titolo inserito non va bene");
+            } else {
+                this.titolo = titolo;
+            }
+        }
+
+        public string GetData() {
+
+            return data.ToShortDateString();
+        }
+
+        public void SetData(string data) {
+            if (DateTime.Parse(data) < DateTime.Now) {
+                throw new Exception("Data inserita non valida");
+            }
+            
+            this.data = DateTime.Parse(data);
+        }
+        public int GetCapienzaMax() {
+            return CapienzaMax;
+        }
+
+        public int GetPostiPrenotati() {
+            return PostiPrenotati;
+        }
+
+        public void PrenotaPosti(int Prenotazione) {
+
+            if (PostiPrenotati == CapienzaMax) {
+                throw new Exception("siamo al completo");
+            } else if (Prenotazione < 0) {
+                throw new Exception("non puoi prenotare meno di 0");
+            } else if ((PostiPrenotati + Prenotazione) > CapienzaMax) {
+                throw new Exception("Mi dispiace non ci sono abbastanza posti");
+            } else {
+                PostiPrenotati += Prenotazione;
+            }
+        }
+        public override string ToString() {
+            string print = "Data: " + GetData()+"\n";
+            print += "Titolo: " + GetTitolo();
+            return print;
+        }
+    }
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,1 +1,18 @@
 ﻿
+
+using GestoreEventi;
+using static System.Net.Mime.MediaTypeNames;
+
+/*try {
+    Evento test = new("le balene", "10/12/22", 12);
+}catch(Exception e) {
+    Console.WriteLine(e);
+}*/
+try {
+    Evento test = new("le balene", "14/12/2022", 12);
+    Console.WriteLine(test.GetData());
+    Console.WriteLine(test);
+}
+catch (Exception e) {
+    Console.WriteLine(e);
+}
