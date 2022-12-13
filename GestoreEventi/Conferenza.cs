e63d1b48444c246277df441621e97e1671b9b360
Pedra96 <darkraidialga@yahoo.it>
2022-12-13 16:19:06 +0100

    Bonus Completato

---
+++ GestoreEventi/Conferenza.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi {
+    internal class Conferenza : Evento {
+        private string relatore;
+        private double prezzo;
+
+        public Conferenza(string relatore, double prezzo, string titolo, string data, int CapienzaMax) : base(titolo, data, CapienzaMax) {
+            this.relatore = relatore;
+            this.prezzo = prezzo;
+        }
+        public void SetRelatore(string relatore) {
+            if (string.IsNullOrEmpty(relatore)) {
+                throw new Exception("Valore non valido");
+            } else {
+                this.relatore = relatore;
+            }
+        }
+        public string GetRelatore() {
+            return this.relatore;
+        }
+        public void SetPrezzo(double prezzo) {
+            if (prezzo < 0) {
+                throw new Exception("Prezzo non valido");
+            } else {
+                this.prezzo = Math.Round(prezzo, 2);
+            }
+        }
+        public double GetPrezzo() {
+            return this.prezzo;
+        }
+        public override string ToString() {
+            string print = base.ToString();
+            print += " - Relatore: " + GetRelatore() + " - Prezzo: " + GetPrezzo()+" euro";
+            return print;
+        }
+
+    }
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -4,6 +4,7 @@ using GestoreEventi;
 using static System.Net.Mime.MediaTypeNames;
 using System.Linq;
 using System.ComponentModel;
+using System;
 
 Console.WriteLine("Inserire titolo del programma");
 string titoloProgramma = Console.ReadLine();
@@ -14,9 +15,8 @@ int NumeroEventi = int.Parse(Console.ReadLine());
 for (int i = 0; i < NumeroEventi; i++) {
     while (true) {
         try {
-
-
-            Console.Write("Inserire un titolo: ");
+            int count = i + 1;
+            Console.Write("Inserire titolo dell "+count+"° evento: ");
             string titolo = Console.ReadLine();
             Console.Write("Inserire data (dd/MM/yyyy): ");
             string data = Console.ReadLine();
@@ -42,6 +42,7 @@ if (input.ToLower() == "si"){
     Console.Write("Inserisci una data per cui vuoi filtrare (dd/MM/yyyy): ");
     input = Console.ReadLine();
     List<Evento> ListaFiltrata = new();
+
     Console.WriteLine("Gli eventi in data " + input + " sono:");
     foreach (var evento in ProgrammaEventi.GetListaEventi()) {
         if (DateOnly.Parse(evento.PrintData()) == DateOnly.Parse(input)) {
@@ -51,6 +52,34 @@ if (input.ToLower() == "si"){
     Functions.StampaListaEvento(ListaFiltrata);
 }
 
-var result = ProgrammaEventi.GetListaEventi().Where(o => o.GetTitolo() == input);
-ProgrammaEventi.GetListaEventi().IndexOf()
+Console.WriteLine("---Bonus---");
+
+Console.WriteLine("Quante Conferenze vuoi creare?");
+int NumeroConferenze = int.Parse(Console.ReadLine());
+for (int i = 0; i < NumeroConferenze; i++) {
+    while (true) {
+        try {
+            int count = i + 1;
+            Console.Write("Inserire titolo dell " + count + "° evento: ");
+            string titolo = Console.ReadLine();
+            Console.Write("Inserire data (dd/MM/yyyy): ");
+            string data = Console.ReadLine();
+            Console.Write("Capienza masssima: ");
+            string capienza = Console.ReadLine();
+            Console.Write("Inserire Relatore: ");
+            string relatore = Console.ReadLine();
+            Console.Write("Prezzo: ");
+            string prezzo = Console.ReadLine();
+            Conferenza Conferenza = new(relatore,double.Parse(prezzo),titolo, data, int.Parse(capienza));
+            ProgrammaEventi.AddEvento(Conferenza);
+            break;
+        }
+        catch (Exception ex) {
+            Console.WriteLine(ex);
+            Console.WriteLine("Errore riprovare");
+        }
+    }
+}
+ProgrammaEventi.GetEventi();
+
 ProgrammaEventi.EmptyList();
